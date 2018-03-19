67911b54d8de8f853be041f478b0838188803f58
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-03-19 15:35:40 +0800

    Changed ServiceFactory by addint support of creating ContactMysqlRepository

--- contacts-demo/me/wendysa/contactsdemo/ServiceFactory.java
+++ contacts-demo/me/wendysa/contactsdemo/ServiceFactory.java
@@ -1,15 +1,28 @@
 package me.wendysa.contactsdemo;
 
 import me.wendysa.contactsdemo.contracts.*;
+import me.wendysa.contactsdemo.models.Contact;
 import me.wendysa.contactsdemo.services.*;
+import me.wendysa.contactsdemo.repositories.ContactMysqlRepository;
 import me.wendysa.contactsdemo.repositories.ContactRepository;
 import me.wendysa.contactsdemo.repositories.ScheduleRepository;
 
 public class ServiceFactory {
-  public static IContactBehaviour getContactService(){
-    return new ContactService(new ContactRepository());
+  public static final String MYSQL_STORAGE_TYPE = "mysql";
+  public static final String JDBC_LOCAL_URL = "jdbc:mysql://localhost:3306/contacts-demo?user=root&password=test123&useSSL=false";
+
+  public static IContactBehaviour getContactService(String storageType){
+    IRepository<Contact> contactRepository = null;
+
+    if (MYSQL_STORAGE_TYPE.equals(storageType)) {
+      contactRepository = new ContactMysqlRepository(JDBC_LOCAL_URL);
+    } else {
+      contactRepository = new ContactRepository();
+    }
+    return new ContactService(contactRepository);
   }
 
+  // public static ISchedulable getScheduleService(String storageType) {
   public static ISchedulable getScheduleService() {
     return new ScheduleService (new ScheduleRepository());
   }
