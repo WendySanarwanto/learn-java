a3c4ed014bd7b97b63e616937ccff8489d92844a
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-03-20 10:48:37 +0800

    Added scripts for creating schedule & scheduleContact tables

--- contacts-demo/ddl.sql
+++ contacts-demo/ddl.sql
@@ -10,3 +10,25 @@ ENGINE=InnoDB
 DEFAULT CHARSET=utf8
 COLLATE=utf8_general_ci ;
 
+-- Create schedule table
+CREATE TABLE `contacts-demo`.schedule (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+	title varchar(100) NOT NULL,
+	beginDate DATE NOT NULL,
+	endDate DATE NOT NULL,
+	description varchar(255) NULL,
+	organiser varchar(20) NOT NULL,
+	CONSTRAINT schedule_PK PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci ;
+
+-- Create ScheduleContact table
+CREATE TABLE `contacts-demo`.scheduleContact (
+	scheduleId BIGINT NOT NULL,
+	contactId INT NOT NULL
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci ;
