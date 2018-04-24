00c24fe03cb96c6e0289d71aa1c052632dd2fb09
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-04-24 14:46:13 +0800

    Added sample which demonstrates jsp:include

--- about-jsp/hello-jsp/deploy.sh
+++ about-jsp/hello-jsp/deploy.sh
@@ -5,14 +5,17 @@ export TOMCAT_CONTAINER_NAME=tomcatdev;
 
 if [ -d "./dist" ]; then
   # Cleanup prior .jsp file(s)
-  docker exec -it $TOMCAT_CONTAINER_NAME sh -c "rm -rf $TARGET_ROOT_DIR/hello.jsp";
+  docker exec -it $TOMCAT_CONTAINER_NAME sh -c "rm $TARGET_ROOT_DIR/hello.jsp";
+  docker exec -it $TOMCAT_CONTAINER_NAME sh -c "rm -rf $TARGET_ROOT_DIR/action-demo";
 
   # Copy *.jsp files inside dist into $TOMCAT_CONTAINER_NAME:TARGET_ROOT_DIR
-  docker cp ./dist/*.jsp $TOMCAT_CONTAINER_NAME:$TARGET_ROOT_DIR;
+  for f in dist/*.jsp; do docker cp $f $TOMCAT_CONTAINER_NAME:$TARGET_ROOT_DIR; done;
+  docker exec -it $TOMCAT_CONTAINER_NAME sh -c "mkdir -p $TARGET_ROOT_DIR/action-demo";
+  for f in dist/action-demo/*.jsp; do docker cp $f $TOMCAT_CONTAINER_NAME:$TARGET_ROOT_DIR/action-demo; done;
 fi
 
-# Copy web.xml file into $TOMCAT_CONTAINER_NAME:$WEB_INF directory
-docker cp ./web.xml $TOMCAT_CONTAINER_NAME:$WEB_INF;
+# # Copy web.xml file into $TOMCAT_CONTAINER_NAME:$WEB_INF directory
+# docker cp ./web.xml $TOMCAT_CONTAINER_NAME:$WEB_INF;
 
 # Shutdown the Tomcat server
 docker exec -it $TOMCAT_CONTAINER_NAME sh -c "./bin/shutdown.sh";
