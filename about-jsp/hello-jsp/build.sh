b0b2bfb4274673b69b3353e8c499eb3693bd90ab
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-05-04 18:12:24 +0800

    Removed tomcat dependency on the hello-jsp project and updates its build script.

--- about-jsp/hello-jsp/build.sh
+++ about-jsp/hello-jsp/build.sh
@@ -2,7 +2,8 @@
 ./clean.sh && \
 
 # Build the application's .java files with its dependencies.
-find . -name "*.java" -print | xargs javac -cp "./dependencies/*;." -d ./dist/bean-demo
+mkdir -p dist/bean-demo;
+find . -name "*.java" -print | xargs javac -cp "../../libs/tomcat/*;." -d ./dist/bean-demo;
 
 # Copy all JSP files into dist
 cp -r ./src/action-demo ./dist/;
